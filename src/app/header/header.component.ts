f837a09d00d30fb3da968d2124daf68bc5b37d84
secr <ch.liefrid@gmail.com>
2024-06-03 13:02:08 +0100

    adding loading indecator

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,21 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+toggleDropdown() {
+throw new Error('Method not implemented.');
+}
+deleteChat() {
+throw new Error('Method not implemented.');
+}
+dropdownOpen: any;
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ChatComponent } from "../chat/chat.component";
+import { LoadingindicatorComponent } from '../loadingindicator/loadingindicator.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [ChatComponent]
+    imports: [ChatComponent,LoadingindicatorComponent]
 })
 export class HomeComponent {
 
